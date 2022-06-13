c68a06808d5eb484d2df043dcebf01e7523d810e
Udit Saroj <udit.saroj2000@gmail.com>
2022-06-14 00:13:21 +0530

    fixing tsparticles error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-import Particles from 'react-tsparticles';
-import { loadFull } from "tsparticles";
+//import Particles from 'react-tsparticles';
+//import { loadFull } from "tsparticles";
 import Navigation from './Components/Navigation/Navigation';
 import Signin from './Components/Signin/Signin';
 import Register from './Components/Register/Register';
@@ -123,21 +123,21 @@ class App extends Component {
   }
 
   render() {
-    const particlesInit = async(main) => {
+    //const particlesInit = async(main) => {
     //console.log(main);
 
     // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
     // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
     // starting from v2 you can add only the features you need reducing the bundle size
-    await loadFull(main);
-  };
+    //await loadFull(main);
+  //};
 
-      const particlesLoaded = (container) => {
+      //const particlesLoaded = (container) => {
     //console.log(container);
-    };
+    //};
     return (
       <div className="App">
-        <Particles className='particles'
+        {/*<Particles className='particles'
           id="tsparticles"
           init={particlesInit}
           loaded={particlesLoaded}
@@ -213,7 +213,7 @@ class App extends Component {
             },
             detectRetina: true,
           }}
-        />
+        />*/}
         <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange}/>
         {this.state.route==='home' 
           ?<div>
